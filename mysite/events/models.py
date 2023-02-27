c422c5f5f43df850edb36b50c20f1bbc8a83498a
Christian Garcia <christiangarcia.cg77@gmail.com>
2023-02-27 08:51:05 -0800

    created event model

--- mysite/events/models.py
+++ mysite/events/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+import string
+import random
+
+def generate_unique_code():
+  length = 6
+
+  while True:
+    code = ''.join(random.choices(string.ascii_uppercase, k=length))
+    if Event.objects.filter(code=code).count() == 0:
+      break
+  
+  return code
+
 
 # Create your models here.
+
+class Event(models.Model):
+  code = models.CharField(max_length=8, default="", unique=True)
+  sport = models.CharField(max_length=15)
+  city = models.CharField(max_length=30)
+  datetime = models.DateTimeField()
+
