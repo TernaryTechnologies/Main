e449d0f2480f9411bba55c5ccb2fe76be811f2ee
Selim <selim_frljuckic@yahoo.com>
2023-02-27 22:21:59 -0800

    Modified App.js and added Index.css
    Modified App.js and added index.css to create a basic layout of a home page

--- mysite/frontend/src/components/App.js
+++ mysite/frontend/src/components/App.js
@@ -7,9 +7,59 @@ export default class App extends Component {
   }
 
   render() {
-    return <h1>Testing React Code</h1>;
+    return (
+      <div>
+        <div className="header">
+          <h1>Pickup Sports Club</h1>
+          <form className="search-bar">
+            <input type="text" placeholder="Search" />
+            <button type="submit">Search</button>
+          </form>
+        </div>
+        <nav className="navbar">
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Events</a></li>
+            <li><a href="#">Teams</a></li>
+            <li><a href="#">Profile</a></li>
+          </ul>
+        </nav>
+        <div className="banner">
+          <h2>Map View</h2>
+        </div>
+        <div className="content-wrapper">
+          <h2>Welcome to My App</h2>
+          <p>This application is meant to connect sports enthusiasts and facilitate pickup games.</p>
+          <button>Get Started</button>
+        </div>
+        <footer>
+          
+        <div className="footer-wrapper">
+          <div className="footer-section">
+            <h3>About Us</h3>
+            <p>We are a pickup sports application dedicated to connecting players and creating communities through sports.</p>
+          </div>
+          <div className="footer-section">
+            <h3>Contact Us</h3>
+            <p>Email: info@pickupsportsapp.com</p>
+            <p>Phone: 555-555-5555</p>
+          </div>
+          <div className="footer-section">
+            <h3>Follow Us</h3>
+            <ul className="social-media-icons">
+              <li><a href="#"><i className="fab fa-facebook"></i></a></li>
+              <li><a href="#"><i className="fab fa-twitter"></i></a></li>
+              <li><a href="#"><i className="fab fa-instagram"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+
+      </div>
+    );
   }
 }
 
 const appDiv = document.getElementById("app");
 render(<App />, appDiv);
+
