dcbaf3ea920bbefa5a01224f4fccd4340650f920
Christian Garcia <christiangarcia.cg77@gmail.com>
2023-02-28 21:00:23 -0800

    added routes

--- mysite/frontend/src/components/App.js
+++ mysite/frontend/src/components/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import { BrowserRouter as Router, Routes, Route, Link, Redirect } from "react-router-dom"
+import HomePage from "./HomePage";
+import CreateEventPage from "./CreateEventPage";
+
 
 export default class App extends Component {
   constructor(props) {
@@ -8,54 +12,12 @@ export default class App extends Component {
 
   render() {
     return (
-      <div>
-        <div className="header">
-          <h1>Pickup Sports Club</h1>
-          <form className="search-bar">
-            <input type="text" placeholder="Search" />
-            <button type="submit">Search</button>
-          </form>
-        </div>
-        <nav className="navbar">
-          <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Events</a></li>
-            <li><a href="#">Teams</a></li>
-            <li><a href="#">Profile</a></li>
-          </ul>
-        </nav>
-        <div className="banner">
-          <h2>Map View</h2>
-        </div>
-        <div className="content-wrapper">
-          <h2>Welcome to My App</h2>
-          <p>This application is meant to connect sports enthusiasts and facilitate pickup games.</p>
-          <button>Get Started</button>
-        </div>
-        <footer>
-          
-        <div className="footer-wrapper">
-          <div className="footer-section">
-            <h3>About Us</h3>
-            <p>We are a pickup sports application dedicated to connecting players and creating communities through sports.</p>
-          </div>
-          <div className="footer-section">
-            <h3>Contact Us</h3>
-            <p>Email: info@pickupsportsapp.com</p>
-            <p>Phone: 555-555-5555</p>
-          </div>
-          <div className="footer-section">
-            <h3>Follow Us</h3>
-            <ul className="social-media-icons">
-              <li><a href="#"><i className="fab fa-facebook"></i></a></li>
-              <li><a href="#"><i className="fab fa-twitter"></i></a></li>
-              <li><a href="#"><i className="fab fa-instagram"></i></a></li>
-            </ul>
-          </div>
-        </div>
-      </footer>
-
-      </div>
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<HomePage />} />
+          <Route path='/create' element={<CreateEventPage />} />
+        </Routes>
+      </Router>
     );
   }
 }
