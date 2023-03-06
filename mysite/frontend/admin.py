90423ae194f9e2334e03ecafbb57bf667e75acd1
SkeltalFlamingo <masonnashcrete@outlook.com>
2023-03-05 16:44:00 -0800

    added import/export functionality in the Django Admin page

--- mysite/frontend/admin.py
+++ mysite/frontend/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+from import_export.admin import ImportExportModelAdmin
+
+class gameAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    ...
+admin.site.register(game, gameAdmin)
--- mysite/frontend/models.py
+++ mysite/frontend/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class game(models.Model):
+    day = models.CharField(max_length=9)
+    lastRegisteredParticipation = models.DateTimeField()
