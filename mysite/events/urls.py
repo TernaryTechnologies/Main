e9fdb7b6933a538ab0735b9d2ffaf6385b901779
Christian Garcia <christiangarcia.cg77@gmail.com>
2023-03-07 12:42:02 -0800

    added view to get events by sport

--- mysite/events/urls.py
+++ mysite/events/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from . import views
-from .views import EventView
+from .views import EventView, GetEventsBySport
 
 urlpatterns = [
   path('', views.index, name='index'),
-  path('sport/<sport>', views.search, name='search'),
+  path('get', GetEventsBySport.as_view()),
+  #path('sport/<sport>', views.search, name='search'),
   path('all', EventView.as_view())
 ]
--- mysite/events/views.py
+++ mysite/events/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from rest_framework import generics
+from rest_framework import generics, status
 from .serializers import EventSerializer
 from .models import Event
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 import csv
 
@@ -11,6 +13,23 @@ import csv
 class EventView(generics.ListAPIView):
   queryset = Event.objects.all()
   serializer_class = EventSerializer
+
+class GetEventsBySport(APIView):
+  serializer_class = EventSerializer
+  lookup_url_kwarg = 'sport'
+
+  def get(self, request, format=None):
+    sport = request.GET.get(self.lookup_url_kwarg)
+    if sport != None:
+      events = Event.objects.filter(sport=sport)
+      if len(events) > 0:
+        data = []
+        for event in events:
+          event_data = EventSerializer(event).data
+          data.append(event_data)
+        return Response(data, status=status.HTTP_200_OK)
+      return Response({'No Events Found': 'Sport Not Found'}, status=status.HTTP_404_NOT_FOUND)
+    return Response({'Bad Request': 'Sport parameter not found in request'}, status=status.HTTP_400_BAD_REQUEST)
 
 def index(request):
   return HttpResponse("Hello, you are at the events index")
