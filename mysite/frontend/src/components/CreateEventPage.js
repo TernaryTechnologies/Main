fff795b5d2385bf3d9c2610299f6c3d2e7fe3710
Christian Garcia <christiangarcia.cg77@gmail.com>
2023-03-12 21:43:22 -0700

    edited createeventpage

--- mysite/frontend/src/components/CreateEventPage.js
+++ mysite/frontend/src/components/CreateEventPage.js
@@ -10,6 +10,7 @@ import {
   RadioGroup,
   FormControlLabel,
 } from "@mui/material";
+import { DatePicker } from '@mui/x-date-pickers';
 import { Link } from "react-router-dom";
 
 export default class CreateEventPage extends Component {
@@ -19,11 +20,66 @@ export default class CreateEventPage extends Component {
 
   render() {
     return (
-      <Grid container spacing={1}>
+      <Grid container spacing={2}>
         <Grid item xs={12} align="center">
           <Typography component="h4" variant="h4">
-            Create An Event
+            Event Details
           </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <FormControl>
+            <TextField
+              required={true}
+              type='string'
+              inputProps={{
+                style: {textAlign: 'center'},
+              }}  
+              />
+            <FormHelperText>
+                <div align='center'>Sport</div>
+            </FormHelperText>
+          </FormControl>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <FormControl>
+            <TextField
+              required={true}
+              type='string'
+              inputProps={{
+                style: {textAlign: 'center'},
+              }}  
+              />
+            <FormHelperText>
+                <div align='center'>City</div>
+            </FormHelperText>
+          </FormControl>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <DatePicker />
+        </Grid>
+        <Grid item xs={12} align="center">
+          <FormControl>
+            <TextField
+              required={true}
+              type='time'
+              inputProps={{
+                style: {textAlign: 'center'},
+              }}
+              />
+            <FormHelperText>
+                <div align='center'>Time</div>
+            </FormHelperText>
+          </FormControl>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Button color='primary' variant='contained'>
+            Create An Event
+          </Button>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Button color='secondary' variant='contained' to='/' component={Link}>
+            Back
+          </Button>
         </Grid>
       </Grid>
     );
