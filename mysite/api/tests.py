e542c365358d6fdfe1eb2fbd6484fa5e2dc23a4a
Christian Garcia <christiangarcia.cg77@gmail.com>
2023-03-16 13:34:57 -0700

    created EventModelTestCase

--- mysite/api/tests.py
+++ mysite/api/tests.py
@@ -1,8 +1,32 @@
 from datetime import datetime
 from django.test import TestCase
-from api.models import Sport, Game
+from api.models import Sport, Game, Event
 
 # Create your tests here.
+
+class EventModelTestCase(TestCase):
+    def setUp(self):
+        self.event = Event.objects.create(
+            sport='soccer',
+            city='pomona',
+            date=datetime.strptime('2023-03-30', '%Y-%m-%d').date(),
+            time=datetime.strptime('16:00', '%H:%M').time(),
+        )
+
+    def test_event_string(self):
+        self.assertEqual(str(self.event), 'Sport:soccer, City:pomona, Date:2023-03-30, Time:16:00:00')
+
+    def test_event_sport(self):
+        self.assertEqual(self.event.sport, 'soccer')
+
+    def test_event_city(self):
+        self.assertEqual(self.event.city, 'pomona')
+
+    def test_event_date(self):
+        self.assertEqual(self.event.date.strftime('%Y-%m-%d'), '2023-03-30')
+
+    def test_game_time(self):
+        self.assertEqual(self.event.time.strftime('%H:%M'), '16:00')
 
 # These tests cover the basic functionality of the Game model:
 # - checking that _str_ method returns expected string representation of a Game instance 
