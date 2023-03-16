616ff22ac0c765c417d607dc902dff6c0e0c118f
Christian Garcia <christiangarcia.cg77@gmail.com>
2023-03-16 13:34:33 -0700

    implemented __str__ for Event

--- mysite/api/models.py
+++ mysite/api/models.py
@@ -4,41 +4,44 @@ import string
 import random
 
 def generate_unique_code():
-  length = 6
+	length = 6
 
-  while True:
-    code = ''.join(random.choices(string.ascii_uppercase, k=length))
-    if Event.objects.filter(code=code).count() == 0:
-      break
-  
-  return code
+	while True:
+		code = ''.join(random.choices(string.ascii_uppercase, k=length))
+		if Event.objects.filter(code=code).count() == 0:
+			break
+
+	return code
 
 
 # Create your models here.
 
 class Event(models.Model):
-  code = models.CharField(max_length=8, default=generate_unique_code, unique=True)
-  sport = models.CharField(max_length=15)
-  city = models.CharField(max_length=30)
-  date = models.DateField(default=date.today)
-  time = models.TimeField(default=time(10, 00))
+	code = models.CharField(max_length=8,
+													default=generate_unique_code,
+													unique=True)
+	sport = models.CharField(max_length=15)
+	city = models.CharField(max_length=30)
+	date = models.DateField(default=date.today)
+	time = models.TimeField(default=time(10, 00))
 
+	def __str__(self):
+		return f'Sport:{self.sport}, City:{self.city}, Date:{self.date}, Time:{self.time}'
 
 class Sport(models.Model):
-    name = models.CharField(max_length=200)
+	name = models.CharField(max_length=200)
 
-    def __str__(self):
-        return self.name
+	def __str__(self):
+		return self.name
 
 class Game(models.Model):
-    name = models.CharField(max_length=200)
-    sport = models.ForeignKey(Sport, on_delete=models.CASCADE)
-    location = models.CharField(max_length=200)
-    date = models.DateField()
-    time = models.TimeField()
-    players = models.CharField(max_length=200)
-    description = models.TextField()
+	name = models.CharField(max_length=200)
+	sport = models.ForeignKey(Sport, on_delete=models.CASCADE)
+	location = models.CharField(max_length=200)
+	date = models.DateField()
+	time = models.TimeField()
+	players = models.CharField(max_length=200)
+	description = models.TextField()
 
-    def __str__(self):
-        return self.name
-
+	def __str__(self):
+		return self.name
