f504088d50db0b297d2fd0f6ece6da02dcef8e0c
Selim <selim_frljuckic@yahoo.com>
2023-03-16 00:57:31 -0700

    Add dateAdd date and time format checks in 'tests.py' and update date and time fields to DateTimeField in models.py
    Added test cases for the Game model in 'tests.py', covering basic functionality such as string representation, sport association, and field values. Also fixed some datetime formatting errors in the tests for 'date' and 'time' fields. Updated the Game model in 'models.py' to use DateField and TimeField instead of CharField for the 'date' and 'time' fields, respectively, to ensure correct type and formatting.

--- mysite/frontend/models.py
+++ mysite/frontend/models.py
@@ -2,7 +2,24 @@ from django.db import models
 
 # Create your models here.
 
+class Sport(models.Model):
+    name = models.CharField(max_length=200)
 
-class game(models.Model):
-    day = models.CharField(max_length=9)
-    lastRegisteredParticipation = models.DateTimeField()
+    def __str__(self):
+        return self.name
+
+class Game(models.Model):
+    name = models.CharField(max_length=200)
+    sport = models.ForeignKey(Sport, on_delete=models.CASCADE)
+    location = models.CharField(max_length=200)
+    date = models.DateField()
+    time = models.TimeField()
+    players = models.CharField(max_length=200)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+# class game(models.Model):
+#     day = models.CharField(max_length=9)
+#     lastRegisteredParticipation = models.DateTimeField()
