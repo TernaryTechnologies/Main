14689d5631e57c3f9a0aad540dffab868423307c
Christian Garcia <christiangarcia.cg77@gmail.com>
2023-04-02 21:13:36 -0700

    installed rest_framework_simplejwt

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
     # added tools
     'rest_framework',
+    'rest_framework_simplejwt',
     'import_export',
 
     # custom apps
@@ -133,3 +134,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
