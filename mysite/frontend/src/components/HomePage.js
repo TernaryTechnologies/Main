b7973557955f536cca9bf3ce6a5aaf6d5e724a9d
Christian Garcia <christiangarcia.cg77@gmail.com>
2023-04-04 19:31:05 -0700

    combined old and new homepage

--- mysite/frontend/src/components/HomePage.js
+++ mysite/frontend/src/components/HomePage.js
@@ -8,7 +8,7 @@ const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 function HomePage() {
   const { state, dispatch } = useContext(AuthContext);
-  const { isAuthenticated, user } = useContext(AuthContext).state;
+  const { isAuthenticated, user } = state;
   const navigate = useNavigate();
 
   const [currLocation, setCurrLocation] = useState({
@@ -70,7 +70,7 @@ function HomePage() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch("/event/");
+        const response = await fetch("/api/all");
         const data = await response.json();
         setNearbyEvents(data);
       } catch (error) {
@@ -145,81 +145,93 @@ function HomePage() {
           ))}
         </ul>
       </nav>
-  
-      <div style={{ display: "flex", height: "400px" }}>
-        
-        <div style={{ width: "50%" }}>
-            <div className="nearby-parks-container">
-              <h2>Nearby Events</h2>
-              <p>
-                There is a Basketball game near you in Pomona going on at 5:00 PM <br></br>
-                Code: "GLRELM" <br></br>
-              </p>
-              <ul>
+
+      {isAuthenticated ? (
+        <div style={{ display: "flex", height: "400px" }}>
+          <div style={{ width: "50%" }}>
+              <div className="nearby-parks-container">
+                <h2>Nearby Events</h2>
+                <ul>
+                  {nearbyEvents.map((event) => (
+                    <li key={event.id}>
+                    <Link to={`/api/all/${event.id}`}>{event.sport}</Link>
+                  </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+
+          <div style={{ width: "50%" }}>
+            <div className="map-container">
+              <GoogleMapReact
+                bootstrapURLKeys={{
+                  key: "AIzaSyB_sMVgUoBDYt8hNkW_cEorXESyE93jOgg",
+                }}
+                center={latitude && longitude ? { lat: latitude, lng: longitude } : undefined}
+                zoom={zoom}
+              >
+                  <AnyReactComponent
+                    lat={latitude}
+                    lng={longitude}
+                    text="You Are Here"
+                  />
                 {nearbyEvents.map((event) => (
-                  <li key={event.id}>
-                  <Link to={`/event/${event.id}`}>{event.sport}</Link>
-                </li>
+                  <AnyReactComponent 
+                    key={event.id}
+                    lat={event.latitude}
+                    lng={event.longitude}
+                    text={event.name}
+                  />
                 ))}
-              </ul>
+              </GoogleMapReact>
             </div>
           </div>
-
-        <div style={{ width: "50%" }}>
-          <div className="map-container">
+        </div>
+        ) : (
+        <div>
+          <div style={{height: '250px', width: '100%'}}>
             <GoogleMapReact
-              bootstrapURLKeys={{
-                key: "AIzaSyB_sMVgUoBDYt8hNkW_cEorXESyE93jOgg",
-              }}
-              center={latitude && longitude ? { lat: latitude, lng: longitude } : undefined}
+              bootstrapURLKeys={{ key: 'AIzaSyB_sMVgUoBDYt8hNkW_cEorXESyE93jOgg' }}
+              center={{lat: latitude,lng: longitude}}
               zoom={zoom}
             >
-                <AnyReactComponent
-                  lat={latitude}
-                  lng={longitude}
-                  text="You Are Here"
-                />
-              {nearbyEvents.map((event) => (
-                <AnyReactComponent 
-                  key={event.id}
-                  lat={event.latitude}
-                  lng={event.longitude}
-                  text={event.name}
-                />
-              ))}
+              <AnyReactComponent
+                lat={latitude}
+                lng={longitude}
+                text="You Are Here"
+              />
             </GoogleMapReact>
           </div>
+          <div className="content-wrapper">
+            <h2>Welcome to the Squad!</h2>
+            <p>This application is meant to connect sports enthusiasts and facilitate pickup games.</p>
+            <Button color="primary" variant="contained" component={Link} to="/register">Get Started</Button>
+          </div>
         </div>
-      </div>
-
-      <div className="content-wrapper">
-        <h2>Welcome to the Squad!</h2>
-        <p>This application is meant to connect sports enthusiasts and facilitate pickup games.</p>
-        <Button color="primary" variant="contained" component={Link} to="/register">Get Started</Button>
-      </div>
+      )}
 
       <footer>
-      <div className="footer-wrapper">
-        <div className="footer-section">
-          <h3>About Us</h3>
-          <p>We are a pickup sports application dedicated to connecting players and creating communities through sports.</p>
+        <div className="footer-wrapper">
+          <div className="footer-section">
+            <h3>About Us</h3>
+            <p>We are a pickup sports application dedicated to connecting players and creating communities through sports.</p>
+          </div>
+          <div className="footer-section">
+            <h3>Contact Us</h3>
+            <p>Email: info@pickupsportsapp.com</p>
+            <p>Phone: 555-555-5555</p>
+          </div>
+          <div className="footer-section">
+            <h3>Follow Us</h3>
+            <ul className="social-media-icons">
+              <li><a href="#"><i className="fab fa-facebook"></i></a></li>
+              <li><a href="#"><i className="fab fa-twitter"></i></a></li>
+              <li><a href="#"><i className="fab fa-instagram"></i></a></li>
+            </ul>
+          </div>
         </div>
-        <div className="footer-section">
-          <h3>Contact Us</h3>
-          <p>Email: info@pickupsportsapp.com</p>
-          <p>Phone: 555-555-5555</p>
-        </div>
-        <div className="footer-section">
-          <h3>Follow Us</h3>
-          <ul className="social-media-icons">
-            <li><a href="#"><i className="fab fa-facebook"></i></a></li>
-            <li><a href="#"><i className="fab fa-twitter"></i></a></li>
-            <li><a href="#"><i className="fab fa-instagram"></i></a></li>
-          </ul>
-        </div>
-      </div>
-    </footer>
-
+      </footer>
+      
     </div>
   );
 }
