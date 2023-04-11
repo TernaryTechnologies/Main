24428699f208e7fd6251df4a1d7901af2e5b04f8
Christian Garcia <christiangarcia.cg77@gmail.com>
2023-04-11 12:46:22 -0700

    added map to Create Event Page

--- mysite/frontend/src/components/CreateEventPage.js
+++ mysite/frontend/src/components/CreateEventPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { format } from 'date-fns'
+import { format } from "date-fns";
 import {
   Checkbox,
   Button,
@@ -10,29 +10,75 @@ import {
   FormControl,
   FormControlLabel,
 } from "@mui/material";
-import { DatePicker, TimePicker } from '@mui/x-date-pickers';
+import { DatePicker, TimePicker } from "@mui/x-date-pickers";
 import { Link } from "react-router-dom";
+import GoogleMapReact from "google-map-react";
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 export default class CreateEventPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      sport: '',
-      date: format(new Date(), 'yyyy/MM/dd'),
-      time: '10:00',
-      latitude: '',
-      longitude: '',
+      sport: "",
+      date: format(new Date(), "yyyy/MM/dd"),
+      time: "10:00",
+      latitude: 34.057919,
+      longitude: -117.821342,
       beginner_friendly: false,
       women_only: false,
+      zoom: 12,
     };
 
     this.handleEventButtonPressed = this.handleEventButtonPressed.bind(this);
     this.handleSportChange = this.handleSportChange.bind(this);
     this.handleLatitudeChange = this.handleLatitudeChange.bind(this);
     this.handleLongitudeChange = this.handleLongitudeChange.bind(this);
-    this.handleBeginnerFriendlyChange = this.handleBeginnerFriendlyChange.bind(this);
+    this.handleBeginnerFriendlyChange =
+      this.handleBeginnerFriendlyChange.bind(this);
     this.handleWomenOnlyChange = this.handleWomenOnlyChange.bind(this);
     this.handleTimeChange = this.handleTimeChange.bind(this);
+  }
+
+  componentDidMount() {
+    this.getUserLocation();
+  }
+
+  getUserLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          this.setState({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          });
+        },
+        (error) => {
+          let errorMessage;
+          switch (error.code) {
+            case error.PERMISSION_DENIED:
+              errorMessage = "User denied the request for Geolocation.";
+              break;
+            case error.POSITION_UNAVAILABLE:
+              errorMessage = "Location information is unavailable.";
+              break;
+            case error.TIMEOUT:
+              errorMessage = "The request to get user location timed out.";
+              break;
+            default:
+              errorMessage = "An unknown error occurred.";
+          }
+          console.log("Error getting location: ", errorMessage);
+        },
+        {
+          enableHighAccuracy: true,
+          timeout: 5000,
+          maximumAge: 0,
+        }
+      );
+    } else {
+      console.log("Geolocation is not supported by this browser.");
+    }
   }
 
   handleSportChange(e) {
@@ -43,7 +89,7 @@ export default class CreateEventPage extends Component {
 
   handleDateChange = (date) => {
     this.setState({
-      date: format(new Date(date), 'yyyy-MM-dd'),
+      date: format(new Date(date), "yyyy-MM-dd"),
     });
   };
 
@@ -58,31 +104,31 @@ export default class CreateEventPage extends Component {
       latitude: e.target.value,
     });
   }
-  
+
   handleLongitudeChange(e) {
     this.setState({
       longitude: e.target.value,
     });
   }
-  
+
   handleBeginnerFriendlyChange(e) {
     this.setState({
       beginner_friendly: e.target.checked,
     });
   }
-  
+
   handleWomenOnlyChange(e) {
     this.setState({
       women_only: e.target.checked,
     });
-  }  
+  }
 
   handleEventButtonPressed() {
     const requestOptions = {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
+        Accept: "application/json",
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         sport_name: this.state.sport,
@@ -93,10 +139,9 @@ export default class CreateEventPage extends Component {
         beginner_friendly: this.state.beginner_friendly,
         women_only: this.state.women_only,
       }),
-      
     };
-    console.log(requestOptions)
-    fetch('/api/create-event', requestOptions)
+    console.log(requestOptions);
+    fetch("/api/create-event", requestOptions)
       .then((response) => {
         if (response.ok) {
           return response.json();
@@ -106,14 +151,13 @@ export default class CreateEventPage extends Component {
       })
       .then((data) => console.log(data))
       .catch((error) => {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       });
   }
-  
 
   render() {
     return (
-      <Grid container spacing={2} style={{ padding: '100px 0' }}>
+      <Grid container spacing={2} style={{ padding: "100px 0" }}>
         <Grid item xs={12} align="center">
           <Typography component="h4" variant="h4">
             Event Details
@@ -123,37 +167,35 @@ export default class CreateEventPage extends Component {
           <FormControl>
             <TextField
               required={true}
-              type='string'
+              type="string"
               onChange={this.handleSportChange}
               inputProps={{
-                style: {textAlign: 'center'},
-              }}  
-              />
+                style: { textAlign: "center" },
+              }}
+            />
             <FormHelperText>
-                <div align='center'>Sport</div>
+              <span align="center">Sport</span>
             </FormHelperText>
           </FormControl>
         </Grid>
         <Grid item xs={12} align="center">
           <FormControl>
-            <DatePicker 
-              onChange={this.handleDateChange}/>
+            <DatePicker onChange={this.handleDateChange} />
           </FormControl>
         </Grid>
         <Grid item xs={12} align="center">
           <FormControl>
             <TextField
               required={true}
-              type='time'
+              type="time"
               onChange={this.handleTimeChange}
-              defaultValue={'10:00'}
+              defaultValue={"10:00"}
               inputProps={{
-                style: {textAlign: 'center'},
-                
+                style: { textAlign: "center" },
               }}
-              />
+            />
             <FormHelperText>
-                <div align='center'>Time</div>
+              <span align="center">Time</span>
             </FormHelperText>
           </FormControl>
         </Grid>
@@ -200,16 +242,43 @@ export default class CreateEventPage extends Component {
           />
         </Grid>
         <Grid item xs={12} align="center">
-          <Button color='primary' variant='contained' onClick={this.handleEventButtonPressed}>
+        <div style={{ height: '400px', width: '100%' }}>
+  <GoogleMapReact
+    bootstrapURLKeys={{
+      key: "AIzaSyB_sMVgUoBDYt8hNkW_cEorXESyE93jOgg",
+    }}
+    center={
+      this.state.latitude && this.state.longitude
+        ? { lat: parseFloat(this.state.latitude), lng: parseFloat(this.state.longitude) }
+        : undefined
+    }
+    zoom={this.state.zoom}
+  >
+    {this.state.latitude && this.state.longitude && (
+      <AnyReactComponent
+        lat={parseFloat(this.state.latitude)}
+        lng={parseFloat(this.state.longitude)}
+        text="You Are Here"
+      />
+    )}
+  </GoogleMapReact>
+</div>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Button
+            color="primary"
+            variant="contained"
+            onClick={this.handleEventButtonPressed}
+          >
             Create An Event
           </Button>
         </Grid>
         <Grid item xs={12} align="center">
-          <Button color='secondary' variant='contained' to='/' component={Link}>
+          <Button color="secondary" variant="contained" to="/" component={Link}>
             Back
           </Button>
         </Grid>
       </Grid>
     );
-  }  
+  }
 }
