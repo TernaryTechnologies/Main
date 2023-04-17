1899b42603a12a7b687b8e4718c5f95d187a7028
Selim Frljuckic <selim_frljuckic@yahoo.com>
2023-04-17 13:10:08 -0700

    Added a few models and unit test cases to the frontend application
    These tests verify that the models are functioning as expected within the context of the frontend application. By testing the expected behavior of each model, the tests help ensure that the frontend application is working as intended and that any changes made to the models will not negatively impact the frontend.

--- mysite/frontend/models.py
+++ mysite/frontend/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+class Sport(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+class Game(models.Model):
+    name = models.CharField(max_length=100)
+    sport = models.ForeignKey(Sport, on_delete=models.CASCADE)
+    location = models.CharField(max_length=100)
+    date = models.DateField()
+
+class Player(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    email = models.EmailField()
+
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+    sport = models.ForeignKey(Sport, on_delete=models.CASCADE)
+    players = models.ManyToManyField(Player)
+
+class GameRegistration(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, null=True, blank=True)
+
+
+
--- mysite/frontend/tests.py
+++ mysite/frontend/tests.py
@@ -1,4 +1,50 @@
 from django.test import TestCase
+from .models import Sport, Game, Player, Team, GameRegistration
+from datetime import date
 
 # Create your tests here.
 
+class SportModelTest(TestCase):
+    def test_sport_model(self):
+        sport = Sport.objects.create(name="Basketball", description="A game played with a ball and hoop.")
+        self.assertEqual(str(sport), "Basketball")
+        self.assertEqual(sport.description, "A game played with a ball and hoop.")
+
+class GameModelTest(TestCase):
+    def test_game_model(self):
+        sport = Sport.objects.create(name="Basketball", description="A game played with a ball and hoop.")
+        game = Game.objects.create(name="Pickup Game", sport=sport, location="Central Park", date=date(2022, 1, 1))
+        self.assertEqual(str(game), "Pickup Game")
+        self.assertEqual(game.location, "Central Park")
+
+class PlayerModelTest(TestCase):
+    def test_player_model(self):
+        player = Player.objects.create(name="John Doe", age=25, email="johndoe@example.com")
+        self.assertEqual(str(player), "John Doe")
+        self.assertEqual(player.age, 25)
+        self.assertEqual(player.email, "johndoe@example.com")
+
+class TeamModelTest(TestCase):
+    def test_team_model(self):
+        sport = Sport.objects.create(name="Basketball", description="A game played with a ball and hoop.")
+        player = Player.objects.create(name="John Doe", age=25, email="johndoe@example.com")
+        team = Team.objects.create(name="Team A", sport=sport)
+        team.players.add(player)
+        self.assertEqual(str(team), "Team A")
+        self.assertEqual(team.players.count(), 1)
+
+class GameRegistrationModelTest(TestCase):
+    def test_game_registration_model(self):
+        sport = Sport.objects.create(name="Basketball", description="A game played with a ball and hoop.")
+        game = Game.objects.create(name="Pickup Game", sport=sport, location="Central Park", date=date(2022, 1, 1))
+        player = Player.objects.create(name="John Doe", age=25, email="johndoe@example.com")
+        registration = GameRegistration.objects.create(game=game, player=player)
+        self.assertEqual(registration.game, game)
+        self.assertEqual(registration.player, player)
+        self.assertIsNone(registration.team)
+
+        team = Team.objects.create(name="Team A", sport=sport)
+        team.players.add(player)
+        registration.team = team
+        registration.save()
+        self.assertEqual(registration.team, team)
