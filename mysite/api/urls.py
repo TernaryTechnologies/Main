cb503ae4a10164932009791c957aedc736841554
Christian Garcia <christiangarcia.cg77@gmail.com>
2023-04-18 12:42:22 -0700

    added urls

--- mysite/api/urls.py
+++ mysite/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
-from .views import EventView, GetEventsBySport, CreateEventView, index
+from .views import EventView, CreateEventView, FilteredEventView, index
 
 urlpatterns = [
   path('', index, name='index'),
-  path('get', GetEventsBySport.as_view()),
   path('all', EventView.as_view(), name='event_list'),
   path('create-event', CreateEventView.as_view(), name='create_event'),
   path('geocode/', views.geocode, name='geocode'),
+  path('reverse_geocode/', views.ReverseGeocodeView.as_view(), name='reverse_geocode'),
+  path('filtered/', FilteredEventView.as_view(), name='filtered_events'),
 ]
