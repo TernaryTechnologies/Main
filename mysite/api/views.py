6fd7768b91392732cc83fe4c0d5b123e5a0b9e6a
Christian Garcia <christiangarcia.cg77@gmail.com>
2023-04-18 12:42:09 -0700

    added views for filtered event and reverse geocode

--- mysite/api/views.py
+++ mysite/api/views.py
@@ -1,12 +1,33 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from django.db.models import Q
 from rest_framework import generics, status
-from .serializers import EventSerializer, CreateEventSerializer, SportSerializer
+from .serializers import EventSerializer, CreateEventSerializer, SportSerializer, ReverseGeocodeSerializer
 from .models import Event, Sport
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 import requests
+import math
+
+def haversine_distance(lat1, lon1, lat2, lon2):
+    # Convert latitude and longitude to radians
+    lat1, lon1, lat2, lon2 = map(math.radians, [lat1, lon1, lat2, lon2])
+
+    # Haversine formula
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+    a = math.sin(dlat / 2) ** 2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2) ** 2
+    c = 2 * math.asin(math.sqrt(a))
+
+    # Earth's radius in miles (approximately)
+    earth_radius_miles = 3958.8
+
+    # Calculate the distance in miles
+    distance = earth_radius_miles * c
+    
+    return distance
+
 
 # Create your views here.
 
@@ -24,22 +45,66 @@ class CreateEventView(APIView):
             print("Serializer errors:", serializer.errors)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class GetEventsBySport(APIView):
-  serializer_class = EventSerializer
-  lookup_url_kwarg = 'sport'
+class FilteredEventView(generics.ListAPIView):
+    serializer_class = EventSerializer
 
-  def get(self, request, format=None):
-      sport = request.GET.get(self.lookup_url_kwarg)
-      if sport != None:
-          events = Event.objects.filter(sport=sport)
-          if len(events) > 0:
-              data = []
-              for event in events:
-                  event_data = EventSerializer(event).data
-                  data.append(event_data)
-              return Response(data, status=status.HTTP_200_OK)
-          return Response({'No Events Found': 'Sport Not Found'}, status=status.HTTP_404_NOT_FOUND)
-      return Response({'Bad Request': 'Sport parameter not found in request'}, status=status.HTTP_400_BAD_REQUEST)
+    def get_queryset(self):
+        queryset = Event.objects.all()
+
+        sport = self.request.query_params.get('sport', None)
+        beginner_friendly = self.request.query_params.get('beginner_friendly', None)
+        women_only = self.request.query_params.get('women_only', None)
+        date = self.request.query_params.get('date', None)
+        time = self.request.query_params.get('time', None)
+        user_lat = float(self.request.query_params.get('user_lat', 0))
+        user_lng = float(self.request.query_params.get('user_lng', 0))
+        range_miles = float(self.request.query_params.get('range', 0))
+
+        filters = Q()
+
+        if sport:
+            filters &= Q(sport__name__iexact=sport)
+
+        if beginner_friendly is not None:
+            if beginner_friendly == 'true':
+                filters &= Q(beginner_friendly=True)
+
+        if women_only is not None:
+            filters &= Q(women_only=(women_only == 'true'))
+
+        if date:
+            filters &= Q(date=date)
+
+        if time:
+            filters &= Q(time=time)
+
+        queryset = queryset.filter(filters)
+
+        if range_miles > 0 and user_lat != 0 and user_lng != 0:
+            # Filter events within the specified range
+            queryset = [event for event in queryset if haversine_distance(user_lat, user_lng, event.latitude, event.longitude) <= range_miles]
+
+        return queryset
+
+class ReverseGeocodeView(APIView):
+    def get(self, request):
+        latitude = request.GET.get('latitude')
+        longitude = request.GET.get('longitude')
+
+        if not latitude or not longitude:
+            return Response({"error": "Missing latitude or longitude parameters"}, status=400)
+
+        response = requests.get(f'https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat={latitude}&lon={longitude}')
+        data = response.json()
+
+        result = {
+            "latitude": float(latitude),
+            "longitude": float(longitude),
+            "address": data['display_name'],
+        }
+
+        serializer = ReverseGeocodeSerializer(result)
+        return Response(serializer.data)
 
 @api_view(['GET'])
 def geocode(request):
@@ -54,7 +119,6 @@ def geocode(request):
     except Exception as e:
         print("Error geocoding address:", e)
         return Response({"error": "Error geocoding address"}, status=500)
-
 
 def index(request):
     return HttpResponse("Hello, you are at the api index")
