990e94b01abc7d32772fdac36498e2a36982bf20
Christian Garcia <christiangarcia.cg77@gmail.com>
2023-05-02 14:16:28 -0700

    commented out debugging middleware

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'api.middleware.DebugAuthMiddleware'
+    #'api.middleware.DebugAuthMiddleware'
 ]
 
 ROOT_URLCONF = 'mysite.urls'
